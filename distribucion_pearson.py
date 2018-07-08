3bc2ad370b86c917ed75410766c6ae396983da6f
Geomar Paul <per3app@gmail.com>
2018-07-08 18:53:24 -0500

    Update distribucion_pearson.py

--- distribucion_pearson.py
+++ distribucion_pearson.py
@@ -36,7 +36,7 @@ y=xm-dst*(be)**0.5;
 VP=[gc,be,alf,y]
 
 #valores obtenidos de la tabla de frecuencias de weibull
-#NOTA: ACOPLAR 'factores_frecuencia_weibull' A 'distribucion_<title>.py'
+#NOTA: ACOPLAR 'factores_frecuencia_weibull.py' A 'distribucion_<title>.py'
 Px=[Px22[49],Px22[79],Px22[89],Px22[94],Px22[97],Px22[98],Px33[198],Px44[498],Px55[998]]
 VAE=[k2[49],k2[79],k2[89],k2[94],k2[97],k2[98],k3[198],k4[498],k5[998]] #var. aleatoria
 #mediante t
